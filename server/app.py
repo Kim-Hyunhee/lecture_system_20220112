15547427b88f5d652d28f5f9e1b7fbc504006762
김현희 <tta_bi@naver.com>
2022-01-12 17:16:18 +0900

    /user - PUT 회원가입 기능추가

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 
 from server.db_connector import DBConnetor
 
-from .api.user import login
+from .api.user import login, sign_up
 from .api.lectures import lecture_test
 
 # DB 연결 정보를 관리하는 클래스 생성 => 객체를 변수에 담아두자
@@ -20,8 +20,13 @@ def create_app():
         # form : 폼데이터에 담겨있는 데이터들
         return login(request.form.to_dict())
     
+    # 회원 가입
+    @app.put("/user")
+    def user_put():
+        return sign_up(request.form.to_dict())
+    
     @app.post("/lecture")
     def lecture_post():
         return lecture_test()
-    
+       
     return app
