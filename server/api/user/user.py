58a0a9d37b1059d237459bcdce117f43bba0f1ae
김현희 <tta_bi@naver.com>
2022-01-12 16:25:52 +0900

    실제 사용자 목록을 응답으로.

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,18 +1,26 @@
 # 로그인 / 회원가입 등 사용자 정보 관련 기능 모아두는 모듈
 # DB 연결 정보 보관 변수를 import
 from server.db_connector import DBConnetor
+from server.model.users import Users
 
 db = DBConnetor()
 
 def test():
     
     # DB의 모든 users 조회 쿼리
-    sql = f"SELECT * FROM users;"
+    sql = "SELECT * FROM users"
     db.cursor.execute(sql)
     all_list = db.cursor.fetchall()
     
-    print(all_list)    
+    all_users = []
+    
+    for row in all_list:
+        # Users(row) : Users형태의 인스턴스  생성 => 함수들도 내장하고 있다.
+        # 인스턴스에게 곧바로 -> get_data_object( ) 실행 명령
+        # 해당 유저의 정보를 활용한 dict가 리턴됨
+        # 곧바로 목록의 append의 재료로 활용
+        all_users.append( Users(row).get_data_object() )  
     
     return {
-        '임시' :'임시'
+        'users' : all_users
     }
