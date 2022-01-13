a60f31c3f19ad50c6b21bb4c2a4ebda4e8093e3d
김현희 <tta_bi@naver.com>
2022-01-13 11:04:25 +0900

    실제 취소 로직 구현
    - DB의 내용에 변경을 주는 행위는 commit() 함수 실행해야함

--- server/api/lectures/lecture.py
+++ server/api/lectures/lecture.py
@@ -39,11 +39,37 @@ def apply_lecture(params):
     db.insertAndCommit(sql)
        
     return{
-        'code' : '수강 신청을 성공했습니다.'
+        'code' : '200',
+        'message' : '수강 신청을 성공했습니다.'
     }
     
 # 수강 취소 - DELETE
 def cancel_apply(params):
+    
+    # 1. 수강 신청을 하지 않은 과목 취소? 불가 400
+    
+    sql = f"SELECT * FROM lecture_user WHERE lecture_id={params['lecture_id']} AND user_id={params['user_id']}"
+    
+    already_apply = db.excuteOne(sql)
+    
+    if not already_apply:
+        return {
+            'code' : 400,
+            'message' : '수강 신청 내역이 없습니다.'
+        }, 400
+        
+    # 향후 - 토큰을 받아내면 내가 신청한 과목만 취소 가능하도록
+    
+    # 2. 실제 신청 내역 삭제 (쿼리 매우 유의)
+    
+    sql = f"DELETE FROM lecture_user WHERE lecture_id={params['lecture_id']} AND user_id={params['user_id']}"
+    
+    
+    # DELETE문도 쿼리 실행 / DB 변경 확정 절차로 INSERT INTO와 동일하게 동작함
+    db.cursor.execute(sql)
+    db.db.commit()
+    
     return {
-        '임시' : '수강 취소 기능'
-    }
+        'code' : 200,
+        'message' : '수강 신청이 취소 되었습니다.'
+            }
