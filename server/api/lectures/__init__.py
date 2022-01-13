8355ea46740b600f9ea559a4707eecfba80d71d0
김현희 <tta_bi@naver.com>
2022-01-13 14:25:56 +0900

    주석만 적음

--- server/api/lectures/__init__.py
+++ server/api/lectures/__init__.py
@@ -1,2 +1,2 @@
 from .lecture import get_all_lectures, select_lecture, delete_lecture
-from .reviews import write_review
+from .review import write_review
--- server/api/lectures/review.py
+++ server/api/lectures/review.py
@@ -2,13 +2,19 @@ from server.model import Reviews
 from server import db
 
 def write_review(params):
+    
+    # 중복 불가
+    sql = f"SELECT * FROM lecture_review WHERE lecture_id={params['lecture_id']} AND user_id={params['user_id']}"
+    # 점수 1~5 사이
+    # 제목 5자 이상
+    # 내용 10자 이상
     sql = f"""
     INSERT INTO lecture_review 
     (lecture_id, user_id, title, content, score) 
     VALUES 
     ({params['lecture_id']}, {params['user_id']}, '{params['title']}', '{params['content']}', {params['score']})"""
     
-    db.excuteOne(sql)
+    db.insertAndCommit(sql)
     return {
         'code' :200,
         'message' :'리뷰 등록이 되었습니다.'
