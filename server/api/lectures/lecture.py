2e14f8709ede84a855f4e2286547269cb0a3dc3e
김현희 <tta_bi@naver.com>
2022-01-13 12:47:11 +0900

    중복 불가 수강 신청

--- server/api/lectures/lecture.py
+++ server/api/lectures/lecture.py
@@ -18,3 +18,29 @@ def get_all_lectures(params):
             'lectures' : lectures
         }    
     }
+    
+# 수강 신청 기능
+def select_lecture(params):
+       
+    # 중복 불가
+    
+    sql = f"SELECT * FROM lecture_user WHERE lecture_id={params['lecture_id']} AND user_id={params['user_id']}"
+    duple_lecture = db.excuteOne(sql)
+    
+    sql = f"INSERT INTO lecture_user VALUES({params['lecture_id']}, {params['user_id']})"
+    db.excuteOne(sql)
+        
+    
+    if not duple_lecture :
+        return{
+            
+            'code' : 200,
+            'message' : '수강 신청 완료'
+    }
+    
+    
+    else:
+        return {
+            'code' :400,
+            'message' :'이미 수강 신청을 했습니다.'
+        }, 400
